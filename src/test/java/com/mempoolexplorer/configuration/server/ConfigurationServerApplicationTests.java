847827037ca7a0fc7633feff11922d8c7986c721
dev7ba <dev7ba@protonmail.com>
2021-11-04 11:43:59 +0100

    Initial commit

---
+++ src/test/java/com/mempoolexplorer/configuration/server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mempoolexplorer.configuration.server;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+/*
+	@Test
+	void contextLoads() {
+	}
+*/
+}
